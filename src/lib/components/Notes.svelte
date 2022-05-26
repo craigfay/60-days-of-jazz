a1b12c20021ff51974dbb558773bfdc1d0b88727
craigfay <craigfay1@gmail.com>
2022-05-25 22:38:13 -0500

    Full width tab

--- src/lib/components/Notes.svelte
+++ src/lib/components/Notes.svelte
@@ -1,69 +1,63 @@
 <script>
-	import { onMount } from 'svelte';
-	let target;
+  import { onMount } from 'svelte';
+  let target;
 
-	export let clef = 'treble';
-	export let timeSignature = '4/4';
+  export let clef = 'treble';
+  export let timeSignature = '4/4';
 
-	// Create the notes
-	export let notes = [
-		// A quarter-note C.
-		{ keys: ['c/4'], duration: '8' },
+  // Create the notes
+  export let notes = [
+    // A quarter-note C.
+    { keys: ['c/4'], duration: '8' },
 
-		{ keys: ['c/4'], duration: '16' },
+    { keys: ['c/4'], duration: '16' },
 
-		{ keys: ['c/4'], duration: '16' },
+    { keys: ['c/4'], duration: '16' },
 
-		// A quarter-note D.
-		{ keys: ['d/4'], duration: 'q' },
+    // A quarter-note D.
+    { keys: ['d/4'], duration: 'q' },
 
-		{ keys: ['b/3'], duration: 'qr' },
+    { keys: ['b/3'], duration: 'qr' },
 
-		// A C-Major chord.
-		{ keys: ['c/4', 'e/4', 'g/4'], duration: 'q' },
-	];
+    // A C-Major chord.
+    { keys: ['c/4', 'e/4', 'g/4'], duration: 'q' },
+  ];
 
-	onMount(async () => {
-		const VF = await import('vexflow');
-		const { Vex } = VF;
-		const { Renderer, Stave } = Vex.Flow;
+  onMount(async () => {
+    const VF = await import('vexflow');
+    const { Vex } = VF;
+    const { Renderer, Stave } = Vex.Flow;
 
-		// Create an SVG renderer and attach it to the DIV element named "boo".
-		const renderer = new Renderer(target, Renderer.Backends.SVG);
+    // Create an SVG renderer and attach it to the DIV element named "boo".
+    const renderer = new Renderer(target, Renderer.Backends.SVG);
 
-		// Configure the rendering context.
-		// renderer.resize(401, 200);
-		const context = renderer.getContext();
+    // Configure the rendering context.
+    // renderer.resize(401, 200);
+    const context = renderer.getContext();
 
-		const stave = new Stave(0, 0, 400);
+    const stave = new Stave(0, 0, 400);
 
-		// Add a clef and time signature.
-		stave.addClef(clef).addTimeSignature(timeSignature);
+    // Add a clef and time signature.
+    stave.addClef(clef).addTimeSignature(timeSignature);
 
-		// Connect it to the rendering context and draw!
-		stave.setContext(context).draw();
+    // Connect it to the rendering context and draw!
+    stave.setContext(context).draw();
 
-		// Create the notes
-		const staveNotes = notes.map(options => new VF.StaveNote(options));
+    // Create the notes
+    const staveNotes = notes.map(options => new VF.StaveNote(options));
 
-		// Create a voice in 4/4 and add above notes
-		const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-		voice.addTickables(staveNotes);
+    // Create a voice in 4/4 and add above notes
+    const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+    voice.addTickables(staveNotes);
 
-		// Format and justify the notes to 400 pixels.
-		new VF.Formatter().joinVoices([voice]).format([voice], 400);
+    // Format and justify the notes to 400 pixels.
+    new VF.Formatter().joinVoices([voice]).format([voice], 400);
 
-		// Render voice
-		voice.draw(context, stave);
-	});
+    // Render voice
+    voice.draw(context, stave);
+  });
 </script>
 
 <div class="w-full">
-	<svg viewBox="0 0 401 125" class="mx-auto w-full" bind:this={target} />
+  <svg viewBox="0 0 401 125" class="mx-auto w-full" bind:this={target} />
 </div>
-
-<style global>
-	svg {
-		width: 100%;
-	}
-</style>
--- src/lib/components/Tab.svelte
+++ src/lib/components/Tab.svelte
@@ -1,46 +1,43 @@
 <script>
-	import { onMount } from 'svelte';
-	let target;
+  import { onMount } from 'svelte';
 
-	onMount(async () => {
-		const VF = await import('vexflow');
+  export let notes = [
+    {
+      positions: [{ str: 3, fret: 7 }],
+      duration: 'q'
+    },
 
-		// Create an SVG renderer and attach it to the DIV element named "boo".
-		const renderer = new VF.Vex.Flow.Renderer(target, VF.Vex.Flow.Renderer.Backends.SVG);
+    {
+      positions: [
+        { str: 2, fret: 10 },
+        { str: 3, fret: 9 },
+        { str: 4, fret: 8 }
+      ],
+      duration: 'q'
+    },
 
-		// Configure the rendering context.
-		renderer.resize(500, 500);
+    {
+      positions: [{ str: 2, fret: 5 }],
+      duration: 'h'
+    },
+  ];
 
-		const context = renderer.getContext();
-		const stave = new VF.TabStave(10, 40, 400);
-		stave.addClef('tab').setContext(context).draw();
+  let target;
 
-		const notes = [
-			// A single note
-			new VF.TabNote({
-				positions: [{ str: 3, fret: 7 }],
-				duration: 'q'
-			}),
+  onMount(async () => {
+    const VF = await import('vexflow');
+    const renderer = new VF.Vex.Flow.Renderer(target, VF.Vex.Flow.Renderer.Backends.SVG);
 
-			// A chord with the note on the 3rd string bent
-			new VF.TabNote({
-				positions: [
-					{ str: 2, fret: 10 },
-					{ str: 3, fret: 9 },
-					{ str: 4, fret: 8 }
-				],
-				duration: 'q'
-			}).addModifier(new VF.Bend('Full'), 1),
+    const context = renderer.getContext();
+    const stave = new VF.TabStave(0, 0, 400);
+    stave.addClef('tab').setContext(context).draw();
 
-			// A single note with a harsh vibrato
-			new VF.TabNote({
-				positions: [{ str: 2, fret: 5 }],
-				duration: 'h'
-			}).addModifier(new VF.Vibrato().setHarsh(true).setVibratoWidth(50), 0)
-		];
+    const tabNotes = notes.map(options => new VF.TabNote(options));
 
-		VF.Formatter.FormatAndDraw(context, stave, notes);
-	});
+    VF.Formatter.FormatAndDraw(context, stave, tabNotes);
+  });
 </script>
 
-<div bind:this={target} />
+<div class="w-full">
+  <svg viewBox="0 0 401 125" class="mx-auto w-full" bind:this={target} />
+</div>
